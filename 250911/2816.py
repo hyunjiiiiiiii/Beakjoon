a0d4491056254c4def169f2eb5fd75840bc13536
이현지 <hyunji@ihyeonjiui-iMac.local>
2025-09-12 15:29:29 +0900

    250912

--- 250911/2816.py
+++ 250911/2816.py
@@ -7,14 +7,32 @@ ch_list = []
 for i in range(N):
     ch_list.append(input())
 
-idx = 0
 
+
+#print(ch_list)
+idx = 0
 for j in ch_list:
+    
     if j == "KBS1":
         idx_1 = idx
-    elif j == "KBS2":
-        idx_2 = idx
+        ch_list.remove("KBS1")
+        ch_list.insert(0, "KBS1")
+        break
     else:
+        idx += 1
         continue
 
-print()
+#print(ch_list)
+
+idx = 0
+for j in ch_list:
+    if j == "KBS2":
+        idx_2 = idx
+        break
+    else:
+        idx += 1
+        continue
+
+
+print("1" * idx_1 + "4" * idx_1, end="")
+print("1" * idx_2 + "4" * (idx_2-1))
